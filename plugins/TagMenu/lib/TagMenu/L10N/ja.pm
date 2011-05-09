94dc1812084226e3184576cefda83e56c4f30078
Alfasado Inc <webmaster@alfasado.jp>
2011-05-09 22:00:44 +0900

    Fixed. YAML error.

--- plugins/TagMenu/lib/TagMenu/L10N/ja.pm
+++ plugins/TagMenu/lib/TagMenu/L10N/ja.pm
@@ -4,7 +4,7 @@ use base 'TagMenu::L10N';
 use vars qw( %Lexicon );
 
 our %Lexicon = (
-    "Show menu 'Tags' for entry, page and asset." => "ブログ記事, ウェブページ, アイテムメニューに'タグ'を表示します。",
+    "Show menu &quot;Tags&quot; for entry, page and asset." => "ブログ記事, ウェブページ, アイテムメニューに&quot;タグ&quot;を表示します。",
 );
 
 1;
